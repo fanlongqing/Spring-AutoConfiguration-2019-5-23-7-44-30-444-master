9f429cb846d56dd82c7fc1538a305cb251c62622
fan <610398596@qq.com>
2019-08-09 14:25:13 +0800

    Initial commit

---
+++ Spring-AutoConfiguration-2019-5-23-7-44-30-444-master/src/main/java/com/zhengdianfang/demo/User.java
@@ -0,0 +1,42 @@
+package com.zhengdianfang.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    @Value("${com.zhengdianfang.demo.user.username}")
+    private String username;
+    @Value("${com.zhengdianfang.demo.user.age}")
+    private int age;
+    @Value("${com.zhengdianfang.demo.user.address}")
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public User setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+}
